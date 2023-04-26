c88dbf8cd05bb5c0f64a25c6fb506685b7df767d
Zhao Wei Kenneth Lim <zhaoweikennethlim@Zhaos-MacBook-Pro.local>
2023-04-27 07:50:33 +0800

    update ppt slides

--- capstone/test.py
+++ capstone/test.py
@@ -6,25 +6,8 @@ from scipy.sparse import csr_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-import io
 
-# Add the Steam icon image
-
-
-st.set_page_config(page_title = "GiantClip", page_icon = "🎮")
-# Create a file uploader widget
-uploaded_file = st.file_uploader("Choose your database", accept_multiple_files=False)
-
-# Check if a file was uploaded
-if uploaded_file is not None:
-    # Read the contents of the file as a bytes object
-    file_contents = uploaded_file.read()
-    
-    # Convert the bytes object to a pandas DataFrame
-    games = pd.read_parquet(io.BytesIO(file_contents))
-else:
-    # If no file was uploaded, use a default database file
-    games = pd.read_parquet("Bigclip.pq")
+games = pd.read_csv("Bigclip.csv")
 st.session_state.page = 0
 def get_recommendations(game_profile, selected_genres=None, games=None):
     
@@ -34,7 +17,7 @@ def get_recommendations(game_profile, selected_genres=None, games=None):
         game_profile += ', '.join(genres_lower)
 
     if games is None:
-        games = pd.read_parquet('Bigclip1.pq')
+        games = pd.read_parquet('Bigclip1.csv')
         
     # Convert the game profile to a sparse matrix using TfidfVectorizer
     tfidf = TfidfVectorizer()
